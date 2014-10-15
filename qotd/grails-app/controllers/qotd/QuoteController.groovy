efd7cb92c4bb65a67c29848d0cd57b5b7e2e5637
Clovis Lemes Ferreira Junior <clovisjunior2009@gmail.com>
2014-10-15 07:10:48 -0300

    Continuação capitulo 1

--- qotd/grails-app/controllers/qotd/QuoteController.groovy
+++ qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -2,9 +2,26 @@ package qotd
 
 class QuoteController {
 
+	static scaffold = true
+
     def index() { }
 
     def home() {
     	render "<h1>Real Programmers do not eat Quiche</h1>"
+    }
+
+    def random() {
+    	def allQuotes = Quote.list()
+    	def randomQuote
+
+    	if(allQuotes.size() > 0){
+    		def randomIdx = new Random().nextInt(allQuotes.size())
+    		randomQuote = allQuotes[randomIdx]
+    	}
+    	else{
+    		randomQuote = new Quote(author: "Anonymous", content: "Real Programmers don't eat much Quiche")
+    	}
+
+    	[quote: randomQuote]
     }
 }
