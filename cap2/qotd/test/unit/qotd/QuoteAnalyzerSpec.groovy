f306cccce74b7eb4937aaf65cd4068be0f3de4db
Clovis Lemes Ferreira Junior <clovis.ferreira@serpro.gov.br>
2014-10-22 14:51:02 -0200

    Finanlização Capítulo 2

--- cap2/qotd/test/unit/qotd/QuoteAnalyzerSpec.groovy
+++ cap2/qotd/test/unit/qotd/QuoteAnalyzerSpec.groovy
@@ -12,6 +12,7 @@ class QuoteAnalyzerSpec extends Specification {
 		new Quote(author: "Glen Smith", content: "Groovy solves all problems")
 	]
   
+  @Unroll
 	def "Total number of quotes"() {
 		given: "An analyzer initialized with known quotes"
 		def analyzer = new QuoteAnalyzer(inputQuotes)
@@ -27,4 +28,20 @@ class QuoteAnalyzerSpec extends Specification {
     []          | 0
     quotes      | 2
 	}
+
+  @Unroll
+  def "Number of quotes per author"() {
+    given: "An analyzer initialized with known quotes"
+    def analyzer = new QuoteAnalyzer(quoteList)
+
+    expect: "The per-author quote count is correct"
+    expected == analyzer.quoteCountPerAuthor
+
+    where:
+    quoteList | expected
+    []        | [:]
+    quotes    | ["Peter Ledbrook": 1, "Glen Smith": 1]
+
+  }
+
 }
